6b1a8d1c0a512390b2287155d68b66d9d36481ef
jonDuke <jonduke90@gmail.com>
2020-03-23 13:27:55 -0700

    set up the routes folder and the new book page

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -2,40 +2,44 @@
 
 from flask import Flask, jsonify, render_template
 
-#from web_app.routes.home_routes import home_routes
-#from web_app.routes.book_routes import book_routes
+from my_app.routes.home_routes import home_routes
+from my_app.routes.book_routes import book_routes
 
 def create_app():
     app = Flask(__name__)
-    #app.register_blueprint(home_routes)
-    #app.register_blueprint(book_routes)
-    @app.route("/")
-    def hello():
-        print("VISITED THE HELLO PAGE")
-        return "Hello World from __init__"
+    app.register_blueprint(home_routes)
+    app.register_blueprint(book_routes)
+    # @app.route("/")
+    # def hello():
+    #     print("VISITED THE HELLO PAGE")
+    #     return "Hello World from __init__"
 
-    @app.route("/about")
-    def about():
-        print("VISITED THE ABOUT PAGE")
-        return "About Me!"
+    # @app.route("/about")
+    # def about():
+    #     print("VISITED THE ABOUT PAGE")
+    #     return "About Me!"
 
-    @app.route("/books")
-    def list_books():
-        books = [
-            {'id': 1, 'title': 'Book 1'},
-            {'id': 2, 'title': 'Book 2'},
-            {'id': 3, 'title': 'Book 3'}
-        ]
-        return render_template("books.html")
+    # @app.route("/books")
+    # def list_books():
+    #     books = [
+    #         {'id': 1, 'title': 'Book 1'},
+    #         {'id': 2, 'title': 'Book 2'},
+    #         {'id': 3, 'title': 'Book 3'}
+    #     ]
+    #     return render_template("books.html", books=books)
 
-    @app.route("/books.json")
-    def list_books_json():
-        books = [
-            {'id': 1, 'title': 'Book 1'},
-            {'id': 2, 'title': 'Book 2'},
-            {'id': 3, 'title': 'Book 3'}
-        ]
-        return jsonify(books)
+    # @app.route("/books.json")
+    # def list_books_json():
+    #     books = [
+    #         {'id': 1, 'title': 'Book 1'},
+    #         {'id': 2, 'title': 'Book 2'},
+    #         {'id': 3, 'title': 'Book 3'}
+    #     ]
+    #     return jsonify(books)
+    
+    # @book_routes.route("/books/new")
+    # def new_book():
+    #     return render_template("new_book.html")
 
     return app
 
