ecf19951b0e3ae525f0c3f80e6c788f59b4f9fdc
jonDuke <jonduke90@gmail.com>
2020-03-26 11:12:30 -0700

    moved flash secret key to .env

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -12,16 +12,16 @@ from my_app.routes.admin_routes import admin_routes
 from my_app.routes.iris_routes import iris_routes
 from my_app.routes.stats_routes import stats_routes
 
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL", default="sqlite:///my_app_12.db")
 def create_app():
     app = Flask(__name__)
 
-    app.config["SECRET_KEY"] = "super secret" # todo: pass from env var (enables flash messaging)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///my_app_12.db"
-    #app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/thedo/lambda_u3/sprint-3/my-web-app/my_app_12.db"
+    app.config["SECRET_KEY"] = os.getenv("FLASH_SECRET")
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     # set up authentication variables for admin controls
-    load_dotenv()
     app.config['BASIC_AUTH_USERNAME'] = os.getenv("ADMIN_NAME")
     app.config['BASIC_AUTH_PASSWORD'] = os.getenv("ADMIN_PASS")
     app.config['BASIC_AUTH_REALM'] = 'admin'
